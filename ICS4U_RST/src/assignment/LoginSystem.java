abec1d52fee389ee1835c48bd346034af63583c0
avery <avery@LorG15>
2024-01-20 20:03:36 -0500

    Finished implementing methods throughout the NYTimesFX client code and
    the corresponding LoginSystem class that would enable for an account
    
    to be created during the duration of the program. The account details
    
    have been saved in a file.

--- ICS4U_RST/src/assignment/LoginSystem.java
+++ ICS4U_RST/src/assignment/LoginSystem.java
@@ -3,28 +3,31 @@ package assignment;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class LoginSystem {
 
 	private ArrayList<Account> accountList;
+	private final String ALL_ACCOUNTS_FILE = "data/accountDB"; 
 
 	public LoginSystem() {
-		accountList = new ArrayList<Account>();
-	    ArrayList<String> names = new ArrayList<>();
-	    ArrayList<String> passwords = new ArrayList<>();
+	    accountList = new ArrayList<>();
 	    
-	    try {
-	        BufferedReader reader = new BufferedReader(new FileReader("data/accountDB"));
+	    try (BufferedReader reader = new BufferedReader(new FileReader(ALL_ACCOUNTS_FILE))) {
 	        String line;
+	        int i = 0;
 	        
 	        while ((line = reader.readLine()) != null) {
-	            names.add(line);
-	            passwords.add(reader.readLine());
+	            String name = line;
+	            String password = reader.readLine();
+	            
+	            Account currentAccount = new Account(name, password);
+	            currentAccount.setAccountIndex(++i);
+	            accountList.add(currentAccount);
 	        }
-	        
-	        reader.close();
 	        
 	        // Error Checking
 	    } catch (FileNotFoundException e) {
@@ -32,13 +35,50 @@ public class LoginSystem {
 	    } catch (IOException a) {
 	        a.printStackTrace();
 	    }
-	    
-	    for (int i = 0; i < names.size(); i++) {
-	        Account currentAccount = new Account(names.get(i), passwords.get(i));
-	        currentAccount.setAccountIndex(i + 1);
-	        accountList.add(currentAccount);
-	    }
 	}
+	
+	/**
+	 * Saves all account information to a specified file.
+	 *
+	 * @param file        The path of the file to which the information will be
+	 *                    saved.
+	 * 
+	 * @param accountInfo The list of accounts whose information needs to be saved.
+	 * 
+	 * @throws FileNotFoundException 
+	 * 		If the specified file cannot be found or created.
+	 * 
+	 * @throws IOException           
+	 * 		If an I/O error occurs while writing to the file.
+	 */
+	public void saveAllAccounts(String file) {
+		// Variables
+		String username, password; // Names 
+
+		try {
+			FileWriter fileWriter = new FileWriter(file); // Initialize FileWriter to write to the file
+			PrintWriter filePrinter = new PrintWriter(fileWriter); // Initialize FileReader to read from the file
+
+			for (Account temp : accountList) { // Goes through all the accounts within the data structure (ArrayList) 
+				// Gets all the pertinent information as described in the variables above through gettors 
+				username = temp.getUsername();
+				password = temp.getPassword();
+				
+				// Prints all the information into the text file 
+				filePrinter.println(username);
+				filePrinter.println(password);
+
+			}
+			fileWriter.close(); // Closes at the end 
+		} catch (FileNotFoundException e) { // If the file is not found
+			e.printStackTrace();
+		} catch (IOException a) { // Handles the IOException
+			a.printStackTrace();
+		} // Again used the .printStackTrace() for the same reasons outlined in the PointsRecorder class 
+	}
+	
+	
+	
 
 	public boolean checkCredentials(String password, String username) {
 	    for (Account account : accountList) {
@@ -48,6 +88,25 @@ public class LoginSystem {
 	    }
 	    return false;
 	}
+	
+	
+	public void createNewAccount(String username, String password) {
+	    // Assuming accountList is an ArrayList<String>
+	    String[] allUserNames = new String[accountList.size()];
+	    for (int i = 0; i < accountList.size(); i++) {
+	        allUserNames[i] = accountList.get(i).getUsername();
+	    }
+	    if (findUserName(allUserNames, username, 0, allUserNames.length - 1)) {
+	    	throw new IllegalArgumentException("This user name is already taken!");
+	    } else {
+	    	Account newAccount = new Account(username, password);
+	    	accountList.add(newAccount); 
+	    }
+
+	
+		
+	}
+	
 
 	public boolean findUserName(String[] allUserNames, String searchName, int left, int right) {
 		if (left > right) {
--- ICS4U_RST/src/assignment/NYTimesFX.java
+++ ICS4U_RST/src/assignment/NYTimesFX.java
@@ -21,12 +21,15 @@ import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.FontWeight;
 
 public class NYTimesFX extends Application {
 
 	private static final int GAP = 15;
 	private static final int LENGTH = 5;
 	private static final int HEIGHT = 6;
+	private static final int LARGE_FONT = 40; 
 	private static final int FONT = 20;
 	private static final int SMALL_FONT = 15;
 	private static final int LOGIN_SCREEN_WIDTH = 400;
@@ -42,11 +45,21 @@ public class NYTimesFX extends Application {
 	private Stage myStage;
 
 	// JavaFX elements
-	private TextField txtUsername, txtPassword;
-	private Button btnSubmit;
+	private TextField txtUsername, txtPassword, txtNewUsername, txtNewPassword;
+	private BackgroundImage homepageBackgroundImage; 
+	private Background homepageBackground; 
 	
+	private String FILE = "data/accountDB";
 	private static int numGuesses = 0; 
+	private boolean gameEnded; 
+	private Scene wordleScene; 
 
+	
+	@Override
+	public void stop() throws Exception { // At the end of the program, save all of the information into the text file 
+		loginCheck.saveAllAccounts(FILE);
+	}
+	
 	@Override
 	public void start(Stage myStage) throws Exception {
 		this.myStage = myStage;
@@ -57,57 +70,65 @@ public class NYTimesFX extends Application {
 		root.setPadding(new Insets(GAP, GAP, GAP, GAP));
 		root.setAlignment(Pos.CENTER);
 
-		Image symbol = new Image("/images/symbol.png");
-		root.add(symbol, 2, 0); // Assuming you want it in the top-right corner (adjust column index as needed)
+		ImageView imgSymbol = new ImageView(getClass().getResource("/images/symbol.png").toString());
+		imgSymbol.setFitWidth(100); // Set the desired width
+		imgSymbol.setFitHeight(100); // Set the desired height
+		root.add(imgSymbol, 0, 0); // Assuming you want it in the top-right corner (adjust column index as needed)
 		
 		Label lblTitle = new Label();
-		lblTitle.setFont(Font.font(FONT));
+		lblTitle.setFont(Font.font("Times New Roman", FontWeight.BOLD, LARGE_FONT)); // Change "Arial" to your desired font family
 		lblTitle.setText("Login Page");
 		root.add(lblTitle, 1, 0, 1, 1);
 		GridPane.setHalignment(lblTitle, HPos.LEFT);
 
 		txtUsername = new TextField();
-		txtUsername.setFont(Font.font(FONT));
+		txtUsername.setFont(Font.font("Times New Roman", FONT));
 		root.add(txtUsername, 1, 1);
 
 		txtPassword = new TextField();
-		txtPassword.setFont(Font.font(FONT));
+		txtPassword.setFont(Font.font("Times New Roman", FONT));
 		root.add(txtPassword, 1, 2);
 
-		btnSubmit = new Button();
-		btnSubmit.setFont(Font.font(FONT));
+		Button btnSubmit = new Button();
+		btnSubmit.setFont(Font.font("Times New Roman",FONT));
 		btnSubmit.setText("Submit");
 		btnSubmit.setOnAction(event -> checkCredentials());
-		root.add(btnSubmit, 1, 3);
+		root.add(btnSubmit, 0, 3, 2, 1);
+		GridPane.setHalignment(btnSubmit, HPos.CENTER);
 
 		Label lblUsername = new Label();
-		lblUsername.setFont(Font.font(FONT));
+		lblUsername.setFont(Font.font("Times New Roman",FONT));
 		lblUsername.setText("Username:");
 		root.add(lblUsername, 0, 1, 1, 1);
 		GridPane.setHalignment(lblUsername, HPos.LEFT);
 
 		Label lblPassword = new Label();
-		lblPassword.setFont(Font.font(FONT));
+		lblPassword.setFont(Font.font("Times New Roman", FONT));
 		lblPassword.setText("Password:");
 		root.add(lblPassword, 0, 2, 1, 1);
 		GridPane.setHalignment(lblPassword, HPos.LEFT);
 
 		Label lblNoAccount = new Label(); 
-		lblNoAccount.setFont(Font.font(SMALL_FONT));
+		lblNoAccount.setFont(Font.font("Times New Roman", SMALL_FONT));
 		lblNoAccount.setText("Do not have an account? Create one below!");
 		root.add(lblNoAccount, 0, 5, 2, 1);  // Set row span to 2
 		GridPane.setHalignment(lblNoAccount, HPos.CENTER);
 		
+		Button btnCreateNewAccount = new Button();
+		btnCreateNewAccount.setFont(Font.font("Times New Roman", FONT)); 
+		btnCreateNewAccount.setText("Create New Account");
+		btnCreateNewAccount.setOnAction(event -> myStage.setScene(getCreateAccountScene()));
+		root.add(btnCreateNewAccount, 0, 6, 2, 1);
+		GridPane.setHalignment(btnCreateNewAccount, HPos.CENTER);
 
 	    // Creating a background image
 		Image image = new Image("/images/loginPageBG.png");
-	    BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
-	    
-
+		homepageBackgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+		homepageBackground = new Background(homepageBackgroundImage);
+	    root.setBackground(homepageBackground);
 		
+	    // Setting the scene 
 		Scene scene = new Scene(root, LOGIN_SCREEN_WIDTH, LOGIN_SCREEN_HEIGHT);
-		Background background = new Background(backgroundImage);
-	    root.setBackground(background);
 		myStage.setTitle("NYTimesFX");
 		myStage.setScene(scene);
 		myStage.show();
@@ -122,6 +143,22 @@ public class NYTimesFX extends Application {
 			myStage.setScene(getHomeScene());
 		} else {
 			showAlert(AlertType.ERROR, "Invalid Credentials", "Please enter a valid username and password.");
+			txtUsername.clear();
+			txtPassword.clear();
+		}
+	}
+	
+	private void createNewAccount() {
+		String newUsername = txtNewUsername.getText();
+		String newPassword = txtNewPassword.getText();
+	
+		try {
+			loginCheck.createNewAccount(newUsername, newPassword);
+			myStage.setScene(getHomeScene());
+		} catch (IllegalArgumentException e) {
+			showAlert(AlertType.ERROR, "Username error", e.getMessage()); 
+			txtUsername.clear();
+			txtPassword.clear();
 		}
 	}
 
@@ -147,11 +184,68 @@ public class NYTimesFX extends Application {
 
 		return new Scene(homeRoot, HOME_SCREEN_WIDTH, HOME_SCREEN_HEIGHT);
 	}
+	
+	private Scene getCreateAccountScene() {
+		GridPane newAccountRoot = new GridPane();
+		newAccountRoot.setHgap(GAP);
+		newAccountRoot.setVgap(GAP);
+		newAccountRoot.setPadding(new Insets(GAP, GAP, GAP, GAP));
+		newAccountRoot.setAlignment(Pos.CENTER);
+
+		ImageView imgSymbol = new ImageView(getClass().getResource("/images/symbol.png").toString());
+		imgSymbol.setFitWidth(100); // Set the desired width
+		imgSymbol.setFitHeight(100); // Set the desired height
+		newAccountRoot.add(imgSymbol, 0, 0); // Assuming you want it in the top-right corner (adjust column index as needed)
+		
+		Label lblTitle = new Label();
+		lblTitle.setFont(Font.font("Times New Roman", FontWeight.BOLD, LARGE_FONT)); // Change "Arial" to your desired font family
+		lblTitle.setText("Create A New Account");
+		newAccountRoot.add(lblTitle, 1, 0, 1, 1);
+		GridPane.setHalignment(lblTitle, HPos.LEFT);
+
+		txtNewUsername = new TextField();
+		txtNewUsername.setFont(Font.font("Times New Roman", FONT));
+		newAccountRoot.add(txtNewUsername, 1, 1);
+
+		txtNewPassword = new TextField();
+		txtNewPassword.setFont(Font.font("Times New Roman", FONT));
+		newAccountRoot.add(txtNewPassword, 1, 2);
+
+		Label lblNewUsername = new Label();
+		lblNewUsername.setFont(Font.font("Times New Roman",FONT));
+		lblNewUsername.setText("Enter Username:");
+		newAccountRoot.add(lblNewUsername, 0, 1, 1, 1);
+		GridPane.setHalignment(lblNewUsername, HPos.LEFT);
+
+		Label lblNewPassword = new Label();
+		lblNewPassword.setFont(Font.font("Times New Roman", FONT));
+		lblNewPassword.setText("Enter Password:");
+		newAccountRoot.add(lblNewPassword, 0, 2, 1, 1);
+		GridPane.setHalignment(lblNewPassword, HPos.LEFT);
+		
+		Button btnCreateNewAccount = new Button();
+		btnCreateNewAccount.setFont(Font.font("Times New Roman", FONT)); 
+		btnCreateNewAccount.setText("Create New Account");
+		btnCreateNewAccount.setOnAction(event -> createNewAccount());
+		newAccountRoot.add(btnCreateNewAccount, 0, 3, 2, 1);
+		GridPane.setHalignment(btnCreateNewAccount, HPos.CENTER);
+		
+		homepageBackground = new Background(homepageBackgroundImage);
+	    newAccountRoot.setBackground(homepageBackground);
+
+		return new Scene(newAccountRoot, HOME_SCREEN_WIDTH, HOME_SCREEN_HEIGHT);
+	}
+	
+
+	
 
 	private Scene getWordleScene() {
 		// Instantiating the WordleFX scene 
 		wordleGame = new WordleFX(); 
 		numGuesses = 0; 
+		
+		// Setting variables 
+		gameEnded = false; 
 		
 		GridPane wordleRoot = new GridPane();
 		wordleRoot.setHgap(GAP);
@@ -175,16 +269,22 @@ public class NYTimesFX extends Application {
 		GridPane.setHalignment(btnReturnHome, HPos.CENTER);
 		wordleRoot.add(btnReturnHome, 2, 7); // Added 1 to the row index
 
-		Scene wordleScene = new Scene(wordleRoot, WORDLE_SCREEN_WIDTH, WORDLE_SCREEN_HEIGHT);
+		wordleScene = new Scene(wordleRoot, WORDLE_SCREEN_WIDTH, WORDLE_SCREEN_HEIGHT);
 		wordleScene.setOnKeyPressed(event -> handleKeyStroke(event));
 
-		return wordleScene; 
+
+		return wordleScene;
 	}
 
 	private void handleKeyStroke(KeyEvent event) {
+		// Variables 
 		boolean letterPlaced = false;
 	    char character = event.getCode().toString().charAt(0);
 	    
+	    // Game ended 
+	    if (gameEnded) {
+	    	return; 
+	    }
 
 	    for (int row = 0; row < HEIGHT; row++) {
 	        for (int col = 0; col < LENGTH; col++) {
@@ -202,15 +302,19 @@ public class NYTimesFX extends Application {
 
 	    if (letterPlaced) {
 	        if (WordleFX.isRowFull(numGuesses, box)) {
-	        	numGuesses++;
+	        	numGuesses++; 
 	            if (wordleGame.wordClean(box, numGuesses - 1)) {
-	            	showAlert(AlertType.INFORMATION, "You Win!", "The word was: "+ wordleGame.getKeyword()); 
+	            	showAlert(AlertType.INFORMATION, "You Win!", "The word was: " + wordleGame.getKeyword()); 
+	            	gameEnded = true; 
 	            } else if (WordleFX.isBoardFull(box)) {
 	            	showAlert(AlertType.INFORMATION, "alert", "The word was: " + wordleGame.getKeyword()); 
+	            	gameEnded = true; 
 	            }
 	        }
 	    }
 	}
+	
+
 
 
 	private void showAlert(AlertType alertType, String title, String message) {
